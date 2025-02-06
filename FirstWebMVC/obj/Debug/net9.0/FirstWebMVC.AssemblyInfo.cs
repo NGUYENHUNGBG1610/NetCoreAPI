eea37fa0b0428633be309802687a316da114d752
NGUYENHUNGBG1610 <2124011662@student.humg.edu.vn>
2025-02-06 20:15:31 +0700

    update_W3CSS

--- FirstWebMVC/obj/Debug/net9.0/FirstWebMVC.AssemblyInfo.cs
+++ FirstWebMVC/obj/Debug/net9.0/FirstWebMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstWebMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d12b6d5d7dce7532ef138d47ad101c3a43722fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+673ef9e48f1f5af190f434e962318a7dc40559ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstWebMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstWebMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
