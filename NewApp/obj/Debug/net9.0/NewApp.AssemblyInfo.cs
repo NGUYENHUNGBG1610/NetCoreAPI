009efb88de19f84a1f9132daa8116e1143e8193d
NGUYENHUNGBG1610 <2124011662@student.humg.edu.vn>
2025-02-11 16:46:30 +0700

    W3CSS

--- NewApp/obj/Debug/net9.0/NewApp.AssemblyInfo.cs
+++ NewApp/obj/Debug/net9.0/NewApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eea37fa0b0428633be309802687a316da114d752")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+818c3d97e41fb5345f9a92587132e99d8adc3f07")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
