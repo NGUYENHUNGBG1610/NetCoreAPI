31e4dc6ed561f8ffc302c80bea9e2553cb9917bc
NGUYENHUNGBG1610 <2124011662@student.humg.edu.vn>
2025-02-12 16:21:24 +0700

    MvcMovie

--- FirstWebMVC/obj/Debug/net9.0/FirstWebMVC.AssemblyInfo.cs
+++ FirstWebMVC/obj/Debug/net9.0/FirstWebMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstWebMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+818c3d97e41fb5345f9a92587132e99d8adc3f07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+009efb88de19f84a1f9132daa8116e1143e8193d")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstWebMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstWebMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
